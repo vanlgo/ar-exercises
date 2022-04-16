4b10a104b9798c567729118a6bfa4f62d1f72cd0
vanlgo <goellevan@gmail.com>
2022-04-16 07:29:05 +0000

    finished exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+# 1. Load the first store (with `id = 1`) from the database and assign it to an instance variable `@store1`.
+# 2. Load the second store from the database and assign it to `@store2`.
+# 3. Update the first store (`@store1`) instance in the database. (Change its name or something.)
+
+@store1 = Store.find_by(id: 1)
+
+@store2 = Store.find_by(id: 2)
+
+@store1.name = "Mississauga"
+@store1.save
